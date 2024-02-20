1e2dc2419f6fa3fd3663f4b8721a3903dbe30c8e
Kyle Jeremiah Govender <129373267+ST10145498@users.noreply.github.com>
2024-02-20 15:58:21 +0200

    practiceTwo

---
+++ parcticeGitTwo/parcticeGitTwo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parcticeGitTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("super duper ");
+        }
+    }
+}
